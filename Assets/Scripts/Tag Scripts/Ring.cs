a1c2810a3e1289b307ed43697fe29305243ce657
MichaelV <michael@liminalvr.com>
2019-05-21 15:18:47 +1000

    restore *.cs and *.meta files to git from lfs

--- Assets/Scripts/Tag Scripts/Ring.cs
+++ Assets/Scripts/Tag Scripts/Ring.cs
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:248c2cf95b6753f3b0810a8db324faaf7c2ea08381c496d33157e19819bda8a0
-size 131
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+	
+}
