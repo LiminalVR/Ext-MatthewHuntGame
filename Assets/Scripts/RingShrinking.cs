d40a196883bd63468827f59c526f1a79e1c840c6
MichaelV <michael@liminalvr.com>
2019-05-23 11:11:24 +1000

    Remove *.cs and *.meta files from the git LFS

--- Assets/Scripts/RingShrinking.cs
+++ Assets/Scripts/RingShrinking.cs
@@ -1,3 +1,79 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:40ee0cd5762dc9f818d554f579d41ae42d56c9e5834b1bdbc0bfb035b9f37d56
-size 2068
+﻿using System.Collections;
+using UnityEngine;
+
+public class RingShrinking : MonoBehaviour
+{
+
+	public GameObject particleSystem;
+	Vector3 MinScale;
+	Vector3 maxscale;
+	bool Reapeatable;
+	public float speed = 2f;
+	public float duration = 2f;
+	public GameObject prefabeparent;
+	public GameObject DyingPrefab;
+	public Vector3 Pos;
+	public bool HasBeenHit = false;
+	public GameObject RingcountCanvas;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	// start the shrink rutine
+	IEnumerator scale()
+	{
+		MinScale = transform.localScale;
+		while (Reapeatable)
+		{
+			yield return Repeatlerp(MinScale, maxscale, duration);
+			yield return Repeatlerp(maxscale, MinScale, duration);
+		}
+
+	}
+
+	public IEnumerator Repeatlerp(Vector3 a, Vector3 b, float time)
+	{
+		float i = 0.0f;
+		float rate = (1.0f / time) * speed;
+		while (i < 1.0f)
+		{
+			i += Time.deltaTime * rate;
+			transform.localScale = Vector3.Lerp(a, b, i);
+			yield return null;
+		}
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		Pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+	// using this to turn on the corutine in order to make the rings shrink and also turn on the particle system and death state
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.GetComponent<Projectile>() == true)
+		{
+			//why does this break the spawning script for the orbs and make the rings freak out ?????????????????????
+			this.GetComponent<SphereCollider>().enabled = false;
+			Instantiate(DyingPrefab, Pos, Quaternion.identity);
+			Reapeatable = true;
+
+			StartCoroutine("scale");
+
+			particleSystem.GetComponent<ParticleSystem>().Play();
+			GameManager.Instance.RingCountUP();
+			Destroy(other.gameObject);
+			GameManager.Instance._RingList.Remove(gameObject);
+			gameObject.GetComponent<AudioSource>().Play();
+			RingcountCanvas.SetActive(true); 
+
+
+			Destroy(prefabeparent, 2);
+			GameManager.Instance.RingsAlive--; 
+			GameManager.Instance._RingList.RemoveAt(0); 
+		}
+	}
+
+}
